c0ffbc2db21bb4731f4ebd2f0c23e212164e6566
Brad <BradJohns94@gmail.com>
2014-10-20 23:14:15 -0400

    Changed connection test response format

--- api/connection.py
+++ api/connection.py
@@ -14,5 +14,6 @@ conn_api = Blueprint('conn_api', __name__)
 @conn_api.route('/TROPIUS/connection/test', methods=['GET'])
 def test_connection():
     """ Respond that the connection was successful """
-    res = {'test': {'ip': request.environ['REMOTE_ADDR']}}
+    res = {'ip': request.environ['REMOTE_ADDR'],
+           'success': True}
     return jsonify(res)
